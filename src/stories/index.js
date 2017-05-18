c33defd8f5cdd54bb7e7427c0d48df76c609b189
Dan Rashid <dan@pecsafety.com>
2017-05-18 15:10:46 -0500

    Add navigation components

--- src/stories/index.js
+++ src/stories/index.js
@@ -3,6 +3,10 @@ import { storiesOf, action, linkTo } from '@kadira/storybook';
 import Button from './Button';
 import Welcome from './Welcome';
 
+import 'semantic-ui-css/semantic.min.css';
+import MainNavigation from '../components/MainNavigation';
+import SecondaryNavigation from '../components/SecondaryNavigation';
+
 storiesOf('Welcome', module)
   .add('to Storybook', () => (
     <Welcome showApp={linkTo('Button')}/>
@@ -14,4 +18,12 @@ storiesOf('Button', module)
   ))
   .add('with some emoji', () => (
     <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>
+  ));
+
+storiesOf('My Components', module)
+  .add('MainNavigation', () => (
+    <MainNavigation />
+  ))
+  .add('SecondaryNavigation', () => (
+    <SecondaryNavigation />
   ));
